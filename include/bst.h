ab183e06a1213a8ae3b7a7fc432dc785f45e51f6
EgorShanin <94217235+EgorShanin@users.noreply.github.com>
2022-06-08 01:18:15 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -3,3 +3,64 @@
 #define INCLUDE_BST_H_
 
 #endif  // INCLUDE_BST_H_
+
+template<typename T>
+class BST {
+ private:
+    struct Node {
+        T value;
+        int count;
+        Node *left;
+        Node *right;
+    };
+    Node root;
+    Node* addNode(Node* root, T& val) {
+        if (root == nullptr) {
+            root = new Node;
+            root->value = val;
+            root->count = 1;
+            root->left = root->right = nullptr;
+        } else if (val < root->value) {
+            root->left = addNode(root->left, val);
+        } else if (val > root->value) {
+            root->right = addNode(root->right, val);
+        } else {
+            root->count++;
+        }
+        return root;
+    }
+    int searchNode(Node* root, T& val) {
+        if (root == nullptr) {
+            return 0;
+        } else if (root->value == val) {
+            return root->count;
+        } else if (root->value > val) {
+            return searchNode(root->left, val);
+        } else {
+            return searchNode(root->right, val);
+        }
+    }
+    int heightTree(Node *root) {
+        if (root == nullptr) {
+            return 0;
+        } else {
+            int L = heightTree(root->left);
+            int R = heightTree(root->right);
+            if (L > R) {
+                return L++;
+            } else {
+                return R++;
+            }
+        }
+    }
+ public:
+    BST(){root = nullptr;}
+    void add(T& value) {
+        root = addNode(root, value);
+    }
+    int search(T& value) {
+        return searchNode(root, value);
+    }
+    int height() {
+        return heightTree(root);
+    }
